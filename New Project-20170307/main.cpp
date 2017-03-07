ebbe915fa761e0d85393fe509863ea2518673f85
DesaMarkovic <desanka.markovic@gmail.com>
2017-03-07 14:22:12 +0000

    Created by Coding Ground Client

---
+++ New Project-20170307/main.cpp
@@ -0,0 +1,27 @@
+#ifndef OOPSheetQ2
+#define OOPSheetQ2
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class mass {
+    public:
+}
+
+class spring {
+    public:
+}
+
+class world {
+    
+}
+
+int main()
+{
+   cout << "Hello World" << endl; 
+   
+   return 0;
+}
+
